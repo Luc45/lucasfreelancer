1bf1077bbe8dd5eee0f404f17fb3f0d4416c7c54
Luc45 <lucasfbustamante@gmail.com>
2014-10-27 13:52:57 -0200

    Atualização 2.0 - Parallax
    Adição de Parallax, Adição de Brainstorm, Adição de UX, UI e Design,
    Adição de Entre em Contato, Remoção de Posso ser Útil

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 	<head>
 		<title>Lucas Bustamante - Freelancer Web BH - Sites e lojas virtuais</title>
 		<!-- CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/fonts.css">
-		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/fonts.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/animate.css">
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
 		
@@ -44,36 +45,51 @@
 					<div id="eu-sou" class="centralizar-vertical-row">
 						<span>Eu sou</span>
 						<div id="freelancer-web">
-							Freelancer Web para agências.
+							Freelancer Web.
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<!-- O que faço -->
-		<div class="container" id="o-que-faco">
-			<div>
+		<!-- Brainstorm -->
+		<div class="container" id="brainstorm">
+			<div class="col-md-6">
+				<div class="thinker" data-bottom-top="bottom:-300px;" data--300-bottom="bottom:0px;"></div>
+			</div>
+			<div class="col-md-6 texto">
+				<div data-bottom-top="opacity: 0;" data--200-bottom-bottom="opacity:1;">
 				Mais que seguir um briefing com atenção infindável
 				aos detalhes técnicos, procuro sempre contribuir
 				para alcançar os melhores resultados para meus
 				clientes, através do resgate das questões fundamentais
 				do objetivo de um website ou loja virtual, que muitas
 				vezes no processo de criação acabam se perdendo.
+				</div>
 			</div>
 		</div>
-		<!-- GitHub -->
-		<div class="container-fluid" id="github">
-			<div class="container no-margin">
-				<div class="titulo">
-					Acompanhe o Desenvolvimento pelo GitHub
+		<!-- Lampada -->
+		<div class="container-fluid" id="lampada" data-290-bottom="background-color: rgb(25,25,25)" data-290-top="background-color: rgb(202, 51, 51);">
+			<div id="anchor-lampada"></div>
+			<div class="container">
+				<div class="col-md-4">
+					<a href="#anchor-lampada"><div class="lampada" data-anchor-target="#lampada" data-290-bottom="background-image:url('/img/lampada_off.svg');" data-290-top="background-image:url('/img/lampada_on.svg');"></div></a>
 				</div>
-				<div class="texto">
-					Cada projeto recebe um repositório privado no GitHub, onde você
-					pode acompanhar detalhadamente o processo de desenvolvimento
-					e até desenvolver em colaboração, se for o caso.
+				<div class="col-md-8" data-anchor-target="#lampada" data-290-bottom="color: rgb(50,50,50)" data-290-top="color: rgb(255, 255, 255);">
+					<div class="titulo">
+						User Experience<br>
+						User Interface<br>
+						e Design
+					</div>
+					<div class="texto">
+						<p>User Experience é um ramo do desenvolvimento web que visa compreender as necessidades do visitante
+						e tornar estas necessidades o ponto norte do desenvolvimento do site.</p>
+						
+						<p>O User Interface é o que dá identidade visual ao projeto, e torna a navegação fluida e natural, <a href="#" data-anchor-target="#lampada" data-290-bottom="color: rgb(50,50,50)" data-290-top="color: rgb(255, 255, 255);">saiba mais.</a></p>
+						
+						<p>Estes conceitos, aliados a um design bem estruturado e executado é o que torna um site agradável ou não de
+						navegar, e proporciona exatamente o que o visitante esperava encontrar!</p>
+					</div>
 				</div>
-				<img src="/img/github.png" src="Freelancer Web BH - Desenvolvimento com GitHub" class="git"/>
-				<div class="rodape-github"></div>
 			</div>
 		</div>
 		<!-- Cérebro -->
@@ -82,24 +98,24 @@
 			<div id="features-do-meu-cerebro">
 				<div class="col-md-6 divtable no-margin">
 					<div class="lista-esquerdo">
-						<ul>
+						<ul data-bottom-top="opacity:0;" data--150-bottom="opacity:1;">
 							<li>HTML</li>
 							<li>CSS</li>
 							<li>JavaScript</li>
 							<li>PHP</li>
 							<li>Linux</li>
-							<li>White Hat Hacking</li>
+							<li>Hacker Ético (White Hat)</li>
 						</ul>
 					</div>
 					<div class="cerebro-esquerdo">
-						<img src="/img/cerebro-esquerdo.svg"/>
+						<img src="/img/cerebro-esquerdo.svg" data-bottom-top="left:-300px;" data--150-bottom="left:0px;"/>
 					</div>
 				</div>
 				<div class="col-md-6 divtable no-margin">
 					<div class="cerebro-direito">
-						<img src="/img/cerebro-direito.svg"/>
+						<img src="/img/cerebro-direito.svg"  data-bottom-top="right:-300px;" data--150-bottom="right:0px;"/>
 					</div>
-					<div class="lista-direito">
+					<div class="lista-direito" data-bottom-top="opacity:0;" data--150-bottom="opacity:1;">
 						<ul>
 							<li>Senso de Design</li>
 							<li>Design Responsivo</li>
@@ -112,7 +128,42 @@
 				</div>
 			</div>
 		</div>
-		<!-- Posso lhe ser útil? -->
+		<!-- GitHub -->
+		<div class="container-fluid" id="github">
+			<div class="container no-margin">
+				<div class="titulo">
+					Acompanhe o Desenvolvimento pelo GitHub
+				</div>
+				<div class="texto">
+					Cada projeto recebe um repositório privado no GitHub, onde você
+					pode acompanhar detalhadamente o processo de desenvolvimento
+					e, no caso de agências, até desenvolver em colaboração.
+				</div>
+				<img src="/img/github.png" src="Freelancer Web BH - Desenvolvimento com GitHub" class="git"/>
+				<!--<div class="rodape-github"></div>-->
+			</div>
+		</div>
+		<!-- Entre em Contato -->
+		<div class="container-fluid no-margin" id="contato">
+		<div class="nuvens"></div>
+		<div class="nuvens2"></div>
+		
+			<div class="container no-margin">
+				<div class="entre-em-contato" data-bottom-top="top:-200px;opacity:0;" data--150-bottom="top:0px;opacity:1;">
+					Entre em Contato
+				</div>
+				<div class="balao" data-bottom-top="top:500px;" data-top="top:0px;"></div>
+				<div class="nuvem" data-bottom-top="bottom:500px;" data-top="bottom:0px;"></div>
+				<div class="contatos" data-bottom-top="opacity:0;" data--150-bottom="opacity:1;">
+					<div class="normal">Envie seu e-mail para</div>
+					<div class="bold">lucas@lucasfreelancer.com.br</div>
+					<div class="normal">Ou ligue para</div>
+					<div class="bold">(31) 9635-3131 <span class="operadora">/vivo</span></div>
+					<div class="bold">(31) 9411-5735 <span class="operadora">/tim</span></div>
+				</div>
+			</div>
+		</div>
+		<!-- Posso lhe ser útil? 
 		<div class="container-fluid" id="posso-ser-util">
 			<div class="container no-margin">
 				<div class="titulo">Posso lhe ser útil?</div>
@@ -142,11 +193,21 @@
 					</div>
 				</form>
 			</div>
-		</div>
+		</div>-->
 		<!-- Javascript -->
 		<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		<script src="/js/skrollr.min.js"></script>
+		<script src="/js/skrollr.stylesheets.min.js"></script>
+		<!--[if lt IE 9]>
+			<script type="text/javascript" src="/js/skrollr.ie.min.js"></script>
+		<![endif]-->
 		<script>
+			//Iniciando Skroll
+			var s = skrollr.init({
+				forceHeight:false
+			}); 
+			// Iniciando Função de SVG/PNG
 			function supportsSVG() {
 			  return !! document.createElementNS && !! document.createElementNS('http://www.w3.org/2000/svg','svg').createSVGRect;  
 			}
@@ -159,6 +220,15 @@
 				}
 			  }
 			}
+			// Iniciando Função de Loop das Nuvens
+			(function conveyorLoop() {
+				$('#contato .nuvens').animate({
+					'background-position': '+=102'
+				}, 1000, 'linear', conveyorLoop);
+				$('#contato .nuvens2').animate({
+					'background-position': '+=102'
+				}, 5000, 'linear', conveyorLoop);
+			}());
 		</script>
 	</body>
 </html>
